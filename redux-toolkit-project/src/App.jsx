62640e0ad0b814db8815c7e5eb05df2e67156afd
akshat <akshatbhatya@gmail.com>
2024-01-02 17:48:38 +0530

    add redux

---
+++ redux-toolkit-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    learn redux 
+    
+    </>
+  )
+}
+
+export default App
---
+++ redux-toolkit-project/src/Features/toDoSlice.js
@@ -0,0 +1,11 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+
+
+const initialState={
+    todo:[
+        {
+            id:1,
+            Text:'hello world'
+        }
+    ]
+}
